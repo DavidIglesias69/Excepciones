0e6a363f1c69b61e244adfeec5122c1dc4d6f263
DavidIglesias69 <david.iglcua@educa.jcyl.es>
2024-02-20 13:37:28 +0100

    .

--- Excepciones/src/Prueba.java
+++ Excepciones/src/Prueba.java
@@ -1,11 +1,23 @@
-
-
 public class Prueba {
+	
+	
 	public static void main(String[] args) throws DavidException {
+
+		String cadena = "Hola soy David";
+		char delimitador = ' ';
 		
-		cadenaLeer ();
+		
+		String[] palabras = split(cadena, delimitador);
+		for (int i = 0; i < palabras.length; i++) {
+			System.out.println(palabras[i]);
+		}
+		
+		
 	}
-		public static double division() throws DavidException{
+	
+	
+	
+	public static double division() throws DavidException{
 		Scanner num1 = new Scanner(System.in);
 		System.out.println("dime el primer numero");
 		int num1= sr.nextInt();
@@ -19,54 +31,54 @@ public class Prueba {
 			return division;
 		}
 	}
- public static int array ()throws DavidException {
-	 System.out.println("dime posicion de la array");
-	 Scanner aa= new Scanner (System.in);
-	 int num1= aa.nextInt();
-	 int [] array={1,2,3,4,5};
-	 if (num1>array.length || num1<0) {
-		 throw new DavidException("esta fuera de la array.");
-	}else {
-		return array[num1];
-	}
-	
-}
-public static void cadenaLeer () throws DavidException{
-	System.out.println("dime una cifra");
-	Scanner ww = new Scanner(System.in);
-	String cadena= ww.nextLine();
-	for (int i = 0; i < cadena.length(); i++) {
-		if (cadena.charAt(i)== '0' || cadena.charAt(i)== '1' ||cadena.charAt(i)== '2' || cadena.charAt(i)== '3' || cadena.charAt(i)== '4' || cadena.charAt(i)== '5' ||cadena.charAt(i)== '6' || cadena.charAt(i)== '7' ||cadena.charAt(i)== '8' || cadena.charAt(i)== '9') {
-			
+	public static int array ()throws DavidException {
+		System.out.println("dime posicion de la array");
+		Scanner aa= new Scanner (System.in);
+		int num1= aa.nextInt();
+		int [] array={1,2,3,4,5};
+		if (num1>array.length || num1<0) {
+			throw new DavidException("esta fuera de la array.");
 		}else {
-			throw new DavidException("hay un caracter que no es un número");
+			return array[num1];
 		}
-		
-		
+
 	}
-	System.out.println(Integer.parseInt(cadena));
+	public static void cadenaLeer () throws DavidException{
+		System.out.println("dime una cifra");
+		Scanner ww = new Scanner(System.in);
+		String cadena= ww.nextLine();
+		for (int i = 0; i < cadena.length(); i++) {
+			if (cadena.charAt(i)== '0' || cadena.charAt(i)== '1' ||cadena.charAt(i)== '2' || cadena.charAt(i)== '3' || cadena.charAt(i)== '4' || cadena.charAt(i)== '5' ||cadena.charAt(i)== '6' || cadena.charAt(i)== '7' ||cadena.charAt(i)== '8' || cadena.charAt(i)== '9') {
+
+			}else {
+				throw new DavidException("hay un caracter que no es un número");
+			}
+
+
+		}
+		System.out.println(Integer.parseInt(cadena));
+	}
+
+	public String[] split (String cadena, char deli) throws DavidException {
+		int contador=0;
+		for (int i = 0; i < cadena.length; i++) {		
+
+			if (cadena.charAt(i)== deli) {
+				contador = cadena.length();
+
+			} else {
+				throw new DavidException("no esta el limitador");
+
+			}
+			String [] array = new String [contador+1];
+			StringBuffer sr = new StringBuffer();
+			if (cadena.charAt(i)==deli) {
+				sr.append()
+			}
+		}
+	}
+
+
+
 }
-
-public String[] split (String cadena, char deli) throws DavidException {
-	int contador=0;
-	for (int i = 0; i < cadena.length; i++) {		
-	
-	if (cadena.charAt(i)== deli) {
-		contador = cadena.length();
-		
-	} else {
-		throw new DavidException("no esta el limitador");
-		
-	}
-	String [] array = new array [contador+1];
-	StringBuffer sr = new StringBuffer();
-	if (cadena.charAt(i)==deli) {
-		sr.append()
-	}
-	}
-}
-
-
-
-	}
 
